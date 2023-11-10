4700bcd09e28f79e521f32adf94a70d83f15a244
magersoft <magervlad@yandex.ru>
2023-11-10 20:16:05 +0200

    igor help

--- components/deleteTask.js
+++ components/deleteTask.js
@@ -7,4 +7,7 @@ export function deleteTask (event, taskBody, taskStub) {
   if (taskBody.children.length === 1) {
     taskStub.classList.remove('todo_body_text--none');
   }
+
+  const numberOfTasksEl = document.querySelector('#sum_task');
+  numberOfTasksEl.innerHTML = taskBody.querySelectorAll('.todo_list-item').length;
 }
--- components/doneTask.js
+++ components/doneTask.js
@@ -1,9 +1,9 @@
 export function doneTask (event) {
   if (event.target.dataset.action === 'done') {
-    const parentElement = event.target.closest('#id');
-    console.log(parentElement);
-    const taskTitle = parentElement.querySelector('#id');
-    taskTitle.classList.add('todo_list-item--done');
-    console.log(taskTitle)
+    const parentElement = event.target.closest('.todo_list-item');
+    parentElement.classList.toggle('todo_list-item--done');
+
+    const numberOfDoneTasksEl = document.querySelector('#done_task');
+    numberOfDoneTasksEl.innerHTML = document.querySelectorAll('.todo_list-item--done').length;
   }
 }
